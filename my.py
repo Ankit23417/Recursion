94c1ccb0a60e01af43af63d31e54c4db93ecf780
Ankit23417 <164024368+Ankit23417@users.noreply.github.com>
2024-10-18 15:17:30 +0530

    Update my.py

--- my.py
+++ my.py
@@ -1,31 +1,16 @@
-'''a= "my name is {} i am from {}"
-b = "india"
-c="ankit"
-print(a.format(c,b))
-print(f"my name is {c} i am from {b}")'''
-
-txt = "for only{price:.2f}doller"
-print(txt.format(price = 874.3333333))
- 
-print(f"{{c}}")
 
 
-def add(a,b): 
-    return a+b
+def factorial(n):
+    if (n == 0 or n == 1):
+        return 1
+    else:
+        return n*factorial(n-1)
 
-a = 2 
-b = 3
-print(add(a,b))
+print(factorial(5))
+print(factorial(6))
+print(factorial(7))
 
-# def factorial(n):
-#     if (n == 0 or n == 1):
-#         return 1
-#     else:
-#         return n*factorial(n-1)
-
-# print(factorial(5))
-# print(factorial(6))
-# print(factorial(7))
+print("Fibonacci Sequence")
 
 def f(n):
     if (n == 1):
@@ -36,3 +21,6 @@ def f(n):
         return f(n-1)+f(n-2)
 
 print(f(9))
+print(f(14))
+print(f(5))
+print(f(3))
